f52b5973ad1ce59652cbb8c9f77ed4d6fad57fbf
SushilDhore <67548707+SushilDhore@users.noreply.github.com>
2024-08-05 18:36:06 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-cd /home/ubuntu/node/angellist-node.js-app && /bin/sh deploy.sh
+#cd /home/ubuntu/node/angellist-node.js-app && /bin/sh deploy.sh
+
+#!/bin/bash
+
+# Navigate to the application directory
+cd /home/ubuntu/node/angellist-node.js-app/scripts/
+
+# Pull the latest Docker images
+sudo docker-compose pull
+
+# Stop and remove existing containers
+sudo docker-compose down
+
+# Start new containers with the updated image
+sudo docker-compose up -d
+
